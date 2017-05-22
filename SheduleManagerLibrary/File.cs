2548eae876ba84f16a0f4d3390b571af644aae3e
freakmiha <freakmiha@users.noreply.github.com>
2017-05-22 19:34:26 +0300

    Создание clr - процедуры для загрузки файлов

--- SheduleManagerLibrary/File.cs
+++ SheduleManagerLibrary/File.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlTypes;
 using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
 using System.IO;
 
 namespace SheduleManagerLibrary
